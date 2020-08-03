14bae32e9fd08018f39ec87eab18f1f7a1c7412c
Sarah Yoo <gayeon1505@gmail.com>
2020-08-02 22:07:04 -0400

    Updates

---
+++ precache-manifest.3a6c11de5f9b1e56efa4398bc37080f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c82700fe798f733f1d08f7fed191561",
+    "url": "/covid19-forecasting-market/index.html"
+  },
+  {
+    "revision": "562b06215a79261b9802",
+    "url": "/covid19-forecasting-market/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d2293b5f23ec47654b7b",
+    "url": "/covid19-forecasting-market/static/css/main.1b0c0556.chunk.css"
+  },
+  {
+    "revision": "562b06215a79261b9802",
+    "url": "/covid19-forecasting-market/static/js/2.9c4cf3ca.chunk.js"
+  },
+  {
+    "revision": "05ab6b54c24ba0b72dfe2f9d5f202d84",
+    "url": "/covid19-forecasting-market/static/js/2.9c4cf3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2293b5f23ec47654b7b",
+    "url": "/covid19-forecasting-market/static/js/main.00fd52ea.chunk.js"
+  },
+  {
+    "revision": "fe102b2819fc20cf5475",
+    "url": "/covid19-forecasting-market/static/js/runtime-main.b0297ce9.js"
+  }
+]);
