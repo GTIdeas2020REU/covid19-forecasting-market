66fd76da2858b63229bb707d8611670385a13a7b
Aniruddha <aniruddha.murali@gmail.com>
2020-08-06 04:10:50 -0400

    Add MSE function

--- backend/evaluate.py
+++ backend/evaluate.py
@@ -1,9 +1,28 @@
 from confirmed import get_us_new_deaths_weekly_avg, get_us_new_deaths
 from get_estimates import get_daily_forecasts
 
-def get_mse():
-    print(get_us_new_deaths_weekly_avg(get_us_new_deaths()))
-    print(' ')
-    print(get_daily_forecasts())
+import json
+from sklearn.metrics import mean_squared_error 
 
-get_mse()
+def get_mse(confirmed, forecasts):
+    #confirmed = json.loads(get_us_new_deaths_weekly_avg(get_us_new_deaths()))
+    #forecasts = get_daily_forecasts()
+    result = dict()
+
+    for model in forecasts.keys():
+        model_dates = forecasts[model]['target_end_date']
+        model_values = forecasts[model]['value']
+
+        confirmed_values = []
+        prediction_values = []
+        for d in model_dates:
+            try:
+                confirmed_values.append(confirmed[d])
+                prediction_values.append(model_values[model_dates.index(d)])
+            except:
+                break
+
+        mse = mean_squared_error(confirmed_values, prediction_values)
+        result[model] = mse
+
+    return result
