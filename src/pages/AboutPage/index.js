3099548174d4915035b71773c88e2053508b8e80
Rachel Ombok <rbo224@nyu.edu>
2020-08-11 23:17:57 -0400

    edit build file

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -19,7 +19,7 @@ class AboutPage extends Component {
                 <h1 style={h1style}>About</h1>
                 <h3></h3>
                 <p style={pstyle}> 
-                    This Aggregate COVID-19 site aims to show various past data readings of coronavirus data, as well as future predictions from various sources. 
+                    This site aims to show various past data readings of coronavirus data, as well as future predictions from various sources. 
                     Different forecasts relating to the COVID-19 pandemic are displayed, and users can make their own predictions about the future trajectory of 
                     factors relating to the pandemic such as daily deaths, hospitalizations and cases.
                     Our mission is to deliver future projections and collected data by providing the best information on the COVID-19 pandemic.
