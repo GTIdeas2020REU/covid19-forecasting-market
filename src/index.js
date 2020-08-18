09e742cc41f021cbe1c537353f55a22a1bc8782b
Rachel Ombok <rbo224@nyu.edu>
2020-08-17 21:54:58 -0400

    readd router

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import {
 } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
-
+/*
+<React.StrictMode>
+    <App />
+  </React.StrictMode>,
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
