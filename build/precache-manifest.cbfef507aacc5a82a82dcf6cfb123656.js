7c5bbeb801a8078a069c5f4618cec030a1f5489e
Rachel Ombok <rbo224@nyu.edu>
2020-08-20 23:48:33 -0400

    readd build

---
+++ build/precache-manifest.cbfef507aacc5a82a82dcf6cfb123656.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975bf2243c3b565818f15785e97650c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "519759e7eb3ea3f5fb4f",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a777c0eb4582384caa71",
+    "url": "/static/css/main.cf7b3fae.chunk.css"
+  },
+  {
+    "revision": "519759e7eb3ea3f5fb4f",
+    "url": "/static/js/2.73377f05.chunk.js"
+  },
+  {
+    "revision": "1e0e3e4f079e879b1b5d8c39b0df90d3",
+    "url": "/static/js/2.73377f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a777c0eb4582384caa71",
+    "url": "/static/js/main.829b134c.chunk.js"
+  },
+  {
+    "revision": "0dd6eaf023079cb9c20ffce06ddeadb2",
+    "url": "/static/js/main.829b134c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e6ddd865befaad84a878764f7140d868",
+    "url": "/static/media/COVIDforecasts-logo-white.e6ddd865.png"
+  }
+]);
