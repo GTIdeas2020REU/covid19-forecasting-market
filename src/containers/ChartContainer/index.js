714927f89ba651fae13c49478545afc3c5013961
Sarah Yoo <gayeon1505@gmail.com>
2020-10-02 13:50:37 -0400

    update chartContainer

--- src/containers/ChartContainer/index.js
+++ src/containers/ChartContainer/index.js
@@ -1,63 +1,75 @@
 import React from 'react';
 import ParentChart from '../../components/ParentChart';
-
-import { cleanConfirmedData, organizeData } from '../../utils/data';
+import MainChart from '../../components/MainChart';
+import { cleanForecastData, organizeData } from '../../utils/data';
+import { Spinner } from 'react-bootstrap';
 
 class ChartContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      forecast: null,
-      orgs: null,
-      confirmed: null,
-      confirmedAvg: null,
-      userPrediction: null,
-      aggregate: null,
-      loggedIn: false
+      compiledData: null,
+      length: 0,
+      loggedIn: false,
+      category: ""
     };
   }
 
-  componentDidMount() {
-    fetch('/us-inc-deaths-forecasts').then(res => res.json()).then(data => {
-      const [results, orgs] = organizeData(data);
-      this.setState({ forecast: results, orgs });
-    });
-    fetch('/user-prediction?category=us_daily_deaths').then(res => res.json()).then(data => {
-        this.setState({ userPrediction: data });
-    });
-    fetch('/us-inc-deaths-confirmed').then(res => res.json()).then(data => {
-        this.setState({ confirmed: data });
-    });
-    fetch('/us-inc-deaths-confirmed-wk-avg').then(res => res.json()).then(data => {
-      this.setState({ confirmedAvg: data });
-    });
-    fetch('/us-agg-inc-deaths').then(res => res.json()).then(data => {
-        this.setState({ aggregate: data });
-    });
+  async componentDidMount() {
+    const {compiledData, category} = this.props;
+    console.log(category)
+    let allData = {}
+    this.setState({compiledData: allData, category: category})
+    compiledData.map(async function(d) {
+      const name = d.name;
+      const data = d.data;
+      fetch(data).then(res => res.json()).then(data => {
+        console.log(d.name, data)
+        if (name == "forecast") {
+          console.log("forecast")
+          data = cleanForecastData(data)
+        }
+        allData[name] = data;
+        this.setState(prevState => {
+          return {length: prevState.length + 1}
+        })
+        console.log("here ", this.state.length);
+      });
+    }.bind(this))
+
     fetch('/login-status/').then(res => res.json()).then(data => {
-        this.setState({ loggedIn: data['logged in'] });
+      this.setState({ loggedIn: data['logged in'] });
     });
   }
 
+  async fetchData(dataList) {
+    let allData = {}
+    dataList.map(async function(d){
+      const data = await fetch(d.data);
+      let jsonData = await data.json();
+      console.log(jsonData);
+      if (d.name == "forecast") {
+        console.log("forecast")
+        jsonData = cleanForecastData(jsonData)
+        console.log("forecast!! ", jsonData)
+      }
+      allData[d.name] = data
+    })
+    console.log("allData!! ", allData);
+    return allData;
+  }
+
   render() {
-    const { forecast, orgs, userPrediction, confirmed, confirmedAvg, aggregate, loggedIn } = this.state;
-    if (!forecast || !orgs || !userPrediction || !confirmed || !aggregate || !loggedIn) return 'Loading...';
-    const isProfile = this.props.isProfile;
+    // const { forecast, orgs, userPrediction, confirmed, confirmedAvg, aggregate, loggedIn } = this.state;
+    // if (!forecast || !orgs || !userPrediction || !confirmed || !aggregate || !loggedIn) return 'Loading...';
+    const {compiledData, length, loggedIn, category} = this.state;
+    if (!compiledData || length != 5 || !loggedIn) return "Loading..."
+    else {
+      console.log(compiledData, loggedIn, length);
+    }
 
     return (
-      <div className="chart-container">
-        <ParentChart
-          forecast={forecast}
-          orgs={orgs}
-          userPrediction={userPrediction}
-          confirmed={confirmed}
-          confirmedAvg={confirmedAvg}
-          aggregate={aggregate}
-          loggedIn={loggedIn}
-          isProfile={isProfile}
-          //category={"us_daily"}
-        />
-      </div>
+      <MainChart compiled={compiledData} loggedIn={loggedIn} category={category}/>
     );
   }
 }
