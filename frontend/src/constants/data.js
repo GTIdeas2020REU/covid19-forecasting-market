22f6e819fa931899fa295d9ea9ad31b344761870
Sarah Yoo <gayeon1505@gmail.com>
2020-10-09 12:58:55 -0400

    add constants

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -2,3 +2,11 @@ export const titles = {
     "us_daily_cases": ["US Daily Cases", "Daily Cases in the US"],
     "us_daily_deaths": ["US Daily Deaths", "Daily Deaths in the US"]
 }
+
+export const forecastIdentifiers = {
+    'Columbia': 'columbia',
+    'Georgia Tech': 'georgia-tech',
+    'UCLA': 'ucla',
+    'IHME': 'ihme',
+    'Youyang Gu': 'youyang-gu'
+}
